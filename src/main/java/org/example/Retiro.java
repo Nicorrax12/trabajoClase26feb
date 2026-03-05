eec7956b84a9f6cc16f9249e56778a5c6d50f8a4
Nicorrax12 <thetodito123@gmail.com>
2026-03-05 02:20:42 -0500

    Refactor Retiro class withdrawal method

--- src/main/java/org/example/Retiro.java
+++ src/main/java/org/example/Retiro.java
@@ -1,16 +1,16 @@
 package org.example;
 
 public class Retiro {
-    public void ejecutar(Cuenta cuenta, double cantidad) {
-        if (cantidad <= 0) {
-            System.out.println("La cantidad a retirar debe ser mayor a $0.");
-        } else if (cantidad > cuenta.getSaldo()) {
-            System.out.println("Fondos insuficientes. Su saldo es solo de $" + cuenta.getSaldo());
+
+    public void ejecutar(Cuenta cuenta, double plata) {
+        if (plata <= 0) {
+            System.out.println("No puedes retirar cero o números negativos.");
+        } else if (plata > cuenta.getSaldo()) {
+            System.out.println("No tienes tanta plata. Tu saldo es $" + cuenta.getSaldo());
         } else {
-            // Resta el dinero y actualiza el sald
-            cuenta.setSaldo(cuenta.getSaldo() - cantidad);
-            System.out.println("Retiro exitoso. Entregando $" + cantidad);
+            double total = cuenta.getSaldo() - plata;
+            cuenta.setSaldo(total);
+            System.out.println("Retiro exitoso.");
         }
     }
-
 }
