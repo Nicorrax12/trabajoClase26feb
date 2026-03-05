39178573c5b6497b73ff60214f92512509163a35
Nicorrax12 <thetodito123@gmail.com>
2026-03-05 09:32:37 -0500

    Update Consulta.java

--- src/main/java/org/example/Consulta.java
+++ src/main/java/org/example/Consulta.java
@@ -3,6 +3,7 @@ package org.example;
 public class Consulta {
 
     public void ejecutar(Cuenta cuenta) {
-        System.out.println("💰 Tienes: $" + cuenta.getSaldo());
+        System.out.println("Tienes: $" + cuenta.getSaldo());
     }
 }
+
